810df0d27d1ce2569caf467b6e59795334f921db
CharlyPey <charly.peyssard@viacesi.fr>
2017-01-21 16:10:11 +0100

    route

---
+++ route.c
@@ -0,0 +1,42 @@
+#include "pbm.h"
+
+void route(int x, int y, int i, int tab[24][31], FILE *file, Pbm *image)
+{
+    i=0;
+    if(y%2==0)
+    {
+        i=1;
+    }
+    x=0;
+
+    strcpy(image->nomImage, "Ditch.pbm");
+    for(y=i; y<24+i; y=y+4)
+    {
+
+        for (x=0; x<29; x=x+23)
+        {
+            file = fopen(image->nomImage, "r");
+            prendreDimensionsImage(file, &image);
+            mettreImageDansTab(file, &image, tab, x, y);
+            fclose(file);
+
+
+        }
+        }
+
+
+        strcpy(image->nomImage, "Macadam.pbm");
+        for(y=i; y<24+i; y=y+4)
+    {
+
+        for(x=11; x<18; x=x+6)
+        {
+            file = fopen(image->nomImage, "r");
+            prendreDimensionsImage(file, &image);
+            mettreImageDansTab(file, &image, tab, x, y);
+            fclose(file);
+
+        }
+        }
+
+}
