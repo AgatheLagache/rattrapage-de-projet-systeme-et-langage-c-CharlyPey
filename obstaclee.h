791ddd6c5fdf7f2145032fa2f81b9e706cacfc00
CharlyPey <charly.peyssard@viacesi.fr>
2017-01-26 09:53:10 +0100

    Delete obstaclee.h

--- obstaclee.h
+++
@@ -1,6 +0,0 @@
-#ifndef OBSTACLEE_H_INCLUDED
-#define OBSTACLEE_H_INCLUDED
-
-int rand_a_b(int a, int b);
-void obs(int* x1, int y, int tab[28][31], FILE *file, Pbm *image);
-#endif // OBSTACLEE_H_INCLUDED
